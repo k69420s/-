2210b96dfa087729ace288993e03601c4ef51497
Otto Edler <otto.edler@chax.at>
2024-05-09 21:19:09 +0200

    .

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ function require_package () {
     local package=$1
 
     # Check if the package is installed
-    if ! command -v "$package" &> /dev/null; then
+    if [ -z "$(apt list $package --installed)" ]; then
         echo "$package is not installed."
 
         # Check if sudo is available
